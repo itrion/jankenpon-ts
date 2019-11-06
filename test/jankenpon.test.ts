c15fc31e063510e262ccfed57d49cd22b2336b7c
Johan Montenegro <johan.montenegro@merrillcorp.com>
2019-11-06 18:26:26 +0000

    rock vs paper wins paper

--- test/jankenpon.test.ts
+++ test/jankenpon.test.ts
@@ -1,9 +1,15 @@
 function jankenpon(one: string, other: string) {
-    return 'Draw'
+    return other === one ?
+        'Draw' :
+        'Paper';
 }
 
 describe('jankenpon', () => {
     test('Rock vs Rock', () => {
         expect(jankenpon('Rock', 'Rock')).toBe('Draw')
+    });
+
+    test('Rock vs Paper', () => {
+        expect(jankenpon('Rock', 'Paper')).toBe('Paper')
     });
 });

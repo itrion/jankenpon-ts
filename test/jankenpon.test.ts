65fc98e234abbcc7799d44559cac1309fef78894
Johan Montenegro <johan.montenegro@merrillcorp.com>
2019-11-06 19:12:30 +0000

    rock vs scissors wins rock

--- test/jankenpon.test.ts
+++ test/jankenpon.test.ts
@@ -1,7 +1,54 @@
-function jankenpon(one: string, other: string) {
-    return other === one ?
-        'Draw' :
-        'Paper';
+interface Shape {
+    vs(other: Rock): string;
+}
+
+type Rock = Shape;
+type Paper = Shape;
+type Scissors = Shape;
+
+const Rock: Rock = {
+    vs: (other: Shape): string => {
+        switch (other) {
+            case Rock: {
+                return 'Draw';
+            }
+            case Paper: {
+                return 'Paper'
+            }
+            case Scissors: {
+                return 'Rock'
+            }
+        }
+        return 'Unknown'
+    }
+};
+
+const Paper = {
+    vs: (other: Shape): string => {
+        return 'Unknown'
+    }
+};
+
+const Scissors = {
+    vs: (other: Shape): string => {
+        return 'Unknown'
+    }
+};
+
+const shapes = {
+    'Rock': Rock,
+    'Paper': Paper,
+    'Scissors': Scissors
+};
+
+type ShapeName = 'Rock' | 'Paper' | 'Scissors'
+
+function shape(shapeName: ShapeName): Shape {
+    return shapes[shapeName]
+}
+
+function jankenpon(one: ShapeName, other: ShapeName) {
+    return shape(one).vs(shape(other))
 }
 
 describe('jankenpon', () => {
@@ -11,5 +58,9 @@ describe('jankenpon', () => {
 
     test('Rock vs Paper', () => {
         expect(jankenpon('Rock', 'Paper')).toBe('Paper')
+    });
+
+    test('Rock vs Scissors', () => {
+        expect(jankenpon('Rock', 'Scissors')).toBe('Rock')
     });
 });
